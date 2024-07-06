a38b17128eb3d7817c897710ff2051d18546da02
Michael Huff <huff600@hotmail.com>
2024-07-05 19:10:54 -0600

    Initial Login and Registration Setup
    New libraries to display flash messages, validate forms, manage sessions. etc.
    - package.json
    
    Session management
    - server.js
    
    Flash messages
    - server.js
    - baseController.js
    - views/index.ejs
    
    Login & Signup pages
    - server.js
    - accountCongroller.js
    - account-model.js
    - account.css
    - accountRoute.js
    - account-validation.js
    - login.ejs
    - register.ejs

--- server.js
+++ server.js
@@ -11,9 +11,35 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const errorRoute = require("./routes/errorRoute")
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Tempaltes
@@ -36,10 +62,12 @@ app.get("/test", function(req, res){
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
-app.use("/inv", utilities.handleErrors(inventoryRoute))
+app.use("/inv", inventoryRoute)
+
+app.use("/account", accountRoute)
 
 // Intentional error route
-app.use("/error", utilities.handleErrors(errorRoute))
+app.use("/error", errorRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
