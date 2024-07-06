be04799bd4cc53ab138025fde290cd6f65bac6ea
Michael Huff <huff600@hotmail.com>
2024-07-06 10:34:59 -0600

    Password hashing and form stickyness
    password hashing
    - package.json
    - accountController.js
    
    Updated css to form so that it could be reused
    - accountController.js
    
    Changed render functions to be reusable by making them dynamic functions
    - accountController.js
    - account-validation.js
    
    Login logic
    - accountRoute.js
    - account-validation.js
    - login.ejs
    
    Updated errors to be dynamic
    - login.ejs
    - register.ejs

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,18 +2,28 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
-const regValidate = require('../utilities/account-validation')
+const formValidate = require('../utilities/account-validation')
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+// Process the login attempt
+router.post(
+  "/login",
+    formValidate.loginRules(),
+    formValidate.checkLoginData,
+    (req, res) => {
+      res.status(200).send('login process')
+    },
+)
+
 // Process the registration data
 router.post(
     "/register",
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
+    formValidate.registationRules(),
+    formValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount),
 )
 
 module.exports = router;
